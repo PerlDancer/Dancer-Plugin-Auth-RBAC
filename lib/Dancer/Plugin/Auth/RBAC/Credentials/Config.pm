f08e167ea92e0404579736e548c92f55fa954804
franck cuny <franck@lumberjaph.net>
2010-12-09 15:44:35 +0100

    refactor: - add _is_user_authorized - add _authorize

--- lib/Dancer/Plugin/Auth/RBAC/Credentials/Config.pm
+++ lib/Dancer/Plugin/Auth/RBAC/Credentials/Config.pm
@@ -6,76 +6,60 @@ use strict;
 use warnings;
 use base qw/Dancer::Plugin::Auth::RBAC::Credentials/;
 
-
 sub authorize {
-    
-    my ($self, $options, @arguments) = @_;
-    my ($login, $password) = @arguments;
+    my ($self, $options, $login, $password) = @_;
     
     my $settings = $Dancer::Plugin::Auth::RBAC::settings;
-    
-    if ($login) {
-    
-    # authorize a new account using supplied credentials
-        
-        my $accounts = $options->{accounts};
-        
-        unless ($password) {
-            $self->errors('login and password are required');
-            return 0;
-        }
-    
-        if (defined $accounts->{$login}) {
-            
-            if (defined $accounts->{$login}->{password}) {
-                
-                if ($accounts->{$login}->{password} =~ /^$password$/) {
-                    
-                    my $session_data = {
-                        id    => $login,
-                        name  => $accounts->{$login}->{name} || ucfirst($login),
-                        login => $login,
-                        roles => [@{$accounts->{$login}->{roles}}],
-                        error => []
-                    };
-                    return $self->credentials($session_data);
-                    
-                }
-                else {
-                    $self->errors('login and/or password is invalid');
-                    return 0;
-                }
-                
-            }
-            else {
-                $self->errors('attempting to access as inaccessible account');
-                return 0;
-            }
-            
-        }
-        else {
-            $self->errors('login and/or password is invalid');
-            return 0;
-        }
-    
+
+    if (!$login) {
+        return $self->_is_user_authorized();
+    }
+
+    if (!$password) {
+        $self->errors('login and password are required');
+        return 0;
+    }
+
+    $self->_authorize($options, $login, $password);
+}
+
+sub _is_user_authorized {
+    my $self = shift;
+
+    # check if current user session is authorized
+    my $user = $self->credentials;
+    if ( ( $user->{id} || $user->{login} ) && !@{ $user->{error} } ) {
+        return $user;
     }
     else {
-        
-    # check if current user session is authorized
-        
-        my $user = $self->credentials;
-        if (($user->{id} || $user->{login}) && !@{$user->{error}}) {
-            
-            return $user;
-            
-        }
-        else {
-            $self->errors('you are not authorized', 'your session may have ended');
-            return 0;
-        }
-        
+        $self->errors( 'you are not authorized',
+            'your session may have ended' );
+        return 0;
     }
-    
+}
+
+sub _authorize {
+    my ($self, $options, $login, $password) = @_;
+
+    if (!$login && !$password) {
+        $self->errors('login and/or password is invalid');
+        return 0;
+    }
+
+    my $accounts = $options->{accounts};
+
+    if ($accounts->{$login}->{password} eq $password) {
+        my $session_data = {
+            id    => $login,
+            name  => $accounts->{$login}->{name} || ucfirst($login),
+            login => $login,
+            roles => [@{$accounts->{$login}->{roles}}],
+            error => []
+        };
+        return $self->credentials($session_data);
+    }
+    $self->errors('login and/or password is invalid');
+    return 0;
 }
 
 1;
